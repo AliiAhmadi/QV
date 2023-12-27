f991a3e716204354598e67d80d6e10edcf8ed531
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-27 22:04:05 +0330

    add test for lexer - passed correctly

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,9 +1,38 @@
 package lexer
 
-// func TestTokens(t *testing.T) {
-// 	inputs := []string {
-// 		"CREATE",
-// 		"TABLE",
+import (
+	"QV/token"
+	"testing"
+)
 
-// 	}
-// }
+func TestTokens(t *testing.T) {
+	t.Parallel()
+	input := `CREATE TABLE hello();`
+
+	tests := []struct {
+		name            string
+		expectedType    token.Type
+		expectedLiteral string
+	}{
+		{"CREATE", token.CREATE, "CREATE"},
+		{"TABLE", token.TABLE, "TABLE"},
+		{"table name", token.NAME, "hello"},
+		{"left parenthesis", token.LPARENTHESIS, "("},
+		{"right parenthesis", token.RPARENTHESIS, ")"},
+		{"semicolon", token.SEMICOLON, ";"},
+	}
+
+	lexer := New(input)
+
+	for i, test := range tests {
+		tok := lexer.NextToken()
+
+		if tok.Type != test.expectedType {
+			t.Fatalf("tests[%d] incorrect type. expected=%s, got=%s", i, test.expectedType, tok.Type)
+		}
+
+		if tok.Literal != test.expectedLiteral {
+			t.Fatalf("tests[%d] incorrect literal. expected=%s, got=%s", i, test.expectedLiteral, tok.Literal)
+		}
+	}
+}
