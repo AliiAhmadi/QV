f570e383f83ca6cd1a246f057536c2552ee1f9e3
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-28 09:37:35 +0330

    debug tokens - add test for full query create table

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -36,3 +36,57 @@ func TestTokens(t *testing.T) {
 		}
 	}
 }
+
+func TestFullCreateTableQuery(t *testing.T) {
+	t.Parallel()
+	query := `
+	CREATE TABLE customers (
+		customerID INTEGER PRIMARY KEY,
+		name TEXT NOT NULL,
+		address TEXT,
+		email TEXT
+	  );
+	`
+
+	toks := []struct {
+		name            string
+		expectedType    token.Type
+		expectedLiteral string
+	}{
+		{"create keyword", token.CREATE, "CREATE"},
+		{"table keyword", token.TABLE, "TABLE"},
+		{"table name", token.NAME, "customers"},
+		{"left parenthesis", token.LPARENTHESIS, "("},
+		{"column name", token.NAME, "customerID"},
+		{"integer keyword", token.INTEGER, "INTEGER"},
+		{"primary keyword", token.PRIMARY, "PRIMARY"},
+		{"key keyword", token.KEY, "KEY"},
+		{"comma", token.COMMA, ","},
+		{"column name", token.NAME, "name"},
+		{"TEXT keyword", token.TEXT, "TEXT"},
+		{"NOT keyword", token.NOT, "NOT"},
+		{"NULL keyword", token.NULL, "NULL"},
+		{"comma", token.COMMA, ","},
+		{"column name", token.NAME, "address"},
+		{"TEXT keyword", token.TEXT, "TEXT"},
+		{"comma", token.COMMA, ","},
+		{"column name", token.NAME, "email"},
+		{"TEXT keyword", token.TEXT, "TEXT"},
+		{"right parenthesis", token.RPARENTHESIS, ")"},
+		{"semicolon", token.SEMICOLON, ";"},
+	}
+
+	lex := New(query)
+
+	for i, tok := range toks {
+		tk := lex.NextToken()
+
+		if tk.Literal != tok.expectedLiteral {
+			t.Fatalf("tests[%d] incorrect literal. expected=%s, got=%s", i, tok.expectedLiteral, tk.Literal)
+		}
+
+		if tk.Type != tok.expectedType {
+			t.Fatalf("tests[%d] incorrect type. expected=%s, got=%s", i, tok.expectedType, tk.Type)
+		}
+	}
+}
--- token/token.go
+++ token/token.go
@@ -13,93 +13,99 @@ type Token struct {
 }
 
 const (
-	CREATE            = "CREATE"
-	TABLE             = "TABLE"
-	INE               = "IF NOT EXISTS"
-	NULL              = "NULL"
-	NOT               = "NOT"
-	UNIQUE            = "UNIQUE"
-	SEMICOLON         = ";"
-	COMMA             = ","
-	PK                = "PRIMARY KEY"
-	LPARENTHESIS      = "("
-	RPARENTHESIS      = ")"
-	INT               = "INT"
-	INTEGER           = "INTEGER"
-	TINYINT           = "TINYINT"
-	SMALLINT          = "SMALLINT"
-	MEDIUMINT         = "MEDIUMINT"
-	BIGINT            = "BIGINT"
-	UNSIGNED_BIG_INT  = "UNSIGNED BIG INT"
-	INT2              = "INT2"
-	INT8              = "INT8"
-	CHARACTER         = "CHARACTER"
-	VARCHAR           = "VARCHAR"
-	VARYING_CHARACTER = "VARYING CHARACTER"
-	NCHAR             = "NCHAR"
-	NATIVE_CHARACTER  = "NATIVE CHARACTER"
-	NVARCHAR          = "NVARCHAR"
-	TEXT              = "TEXT"
-	CLOB              = "CLOB"
-	BLOB              = "BLOB"
-	REAL              = "REAL"
-	DOUBLE            = "DOUBLE"
-	DOUBLE_PRECISION  = "DOUBLE PRECISION"
-	FLOAT             = "FLOAT"
-	NUMERIC           = "NUMERIC"
-	DECIMAL           = "DECIMAL"
-	BOOLEAN           = "BOOLEAN"
-	DATE              = "DATE"
-	DATETIME          = "DATETIME"
-	DEFAULT           = "DEFAULT"
-	TRUE              = "TRUE"
-	FALSE             = "FALSE"
-	NAME              = "NAME"
-	NUMBER            = "NUMBER"
-	EOQ               = "EOQ"
-	ILLEGAL           = "ILLEGAL"
-	STRING            = "\""
-	BYTE              = "'"
+	CREATE       = "CREATE"
+	TABLE        = "TABLE"
+	IF           = "IF"
+	EXISTS       = "EXISTS"
+	NULL         = "NULL"
+	NOT          = "NOT"
+	UNIQUE       = "UNIQUE"
+	SEMICOLON    = ";"
+	COMMA        = ","
+	KEY          = "KEY"
+	PRIMARY      = "PRIMARY"
+	LPARENTHESIS = "("
+	RPARENTHESIS = ")"
+	INT          = "INT"
+	INTEGER      = "INTEGER"
+	TINYINT      = "TINYINT"
+	SMALLINT     = "SMALLINT"
+	MEDIUMINT    = "MEDIUMINT"
+	BIGINT       = "BIGINT"
+	UNSIGNED     = "UNSIGNED"
+	BIG          = "BIG"
+	INT2         = "INT2"
+	INT8         = "INT8"
+	CHARACTER    = "CHARACTER"
+	VARCHAR      = "VARCHAR"
+	VARYING      = "VARYING"
+	NCHAR        = "NCHAR"
+	NATIVE       = "NATIVE"
+	NVARCHAR     = "NVARCHAR"
+	TEXT         = "TEXT"
+	CLOB         = "CLOB"
+	BLOB         = "BLOB"
+	REAL         = "REAL"
+	DOUBLE       = "DOUBLE"
+	PRECISION    = "PRECISION"
+	FLOAT        = "FLOAT"
+	NUMERIC      = "NUMERIC"
+	DECIMAL      = "DECIMAL"
+	BOOLEAN      = "BOOLEAN"
+	DATE         = "DATE"
+	DATETIME     = "DATETIME"
+	DEFAULT      = "DEFAULT"
+	TRUE         = "TRUE"
+	FALSE        = "FALSE"
+	NAME         = "NAME"
+	NUMBER       = "NUMBER"
+	EOQ          = "EOQ"
+	ILLEGAL      = "ILLEGAL"
+	STRING       = "\""
+	BYTE         = "'"
 )
 
 var keywords map[string]Type = map[string]Type{
-	"CREATE":            CREATE,
-	"TABLE":             TABLE,
-	"IF NOT EXISTS":     INE,
-	"NULL":              NULL,
-	"NOT":               NOT,
-	"UNIQUE":            UNIQUE,
-	"PRIMARY KEY":       PK,
-	"INT":               INT,
-	"INTEGER":           INTEGER,
-	"TINYINT":           TINYINT,
-	"SMALLINT":          SMALLINT,
-	"MEDIUMINT":         MEDIUMINT,
-	"BIGINT":            BIGINT,
-	"UNSIGNED BIG INT":  UNSIGNED_BIG_INT,
-	"INT2":              INT2,
-	"INT8":              INT8,
-	"CHARACTER":         CHARACTER,
-	"VARCHAR":           VARCHAR,
-	"VARYING CHARACTER": VARYING_CHARACTER,
-	"NCHAR":             NCHAR,
-	"NATIVE CHARACTER":  NATIVE_CHARACTER,
-	"NVARCHAR":          NVARCHAR,
-	"TEXT":              TEXT,
-	"CLOB":              CLOB,
-	"BLOB":              BLOB,
-	"REAL":              REAL,
-	"DOUBLE":            DOUBLE,
-	"DOUBLE PRECISION":  DOUBLE_PRECISION,
-	"FLOAT":             FLOAT,
-	"NUMERIC":           NUMERIC,
-	"DECIMAL":           DECIMAL,
-	"BOOLEAN":           BOOLEAN,
-	"DATE":              DATE,
-	"DATETIME":          DATETIME,
-	"DEFAULT":           DEFAULT,
-	"TRUE":              TRUE,
-	"FALSE":             FALSE,
+	"CREATE":    CREATE,
+	"TABLE":     TABLE,
+	"IF":        IF,
+	"EXISTS":    EXISTS,
+	"NULL":      NULL,
+	"NOT":       NOT,
+	"UNIQUE":    UNIQUE,
+	"PRIMARY":   PRIMARY,
+	"KEY":       KEY,
+	"INT":       INT,
+	"INTEGER":   INTEGER,
+	"TINYINT":   TINYINT,
+	"SMALLINT":  SMALLINT,
+	"MEDIUMINT": MEDIUMINT,
+	"BIGINT":    BIGINT,
+	"UNSIGNED":  UNSIGNED,
+	"BIG":       BIG,
+	"INT2":      INT2,
+	"INT8":      INT8,
+	"CHARACTER": CHARACTER,
+	"VARCHAR":   VARCHAR,
+	"VARYING":   VARYING,
+	"NCHAR":     NCHAR,
+	"NATIVE":    NATIVE,
+	"NVARCHAR":  NVARCHAR,
+	"TEXT":      TEXT,
+	"CLOB":      CLOB,
+	"BLOB":      BLOB,
+	"REAL":      REAL,
+	"DOUBLE":    DOUBLE,
+	"PRECISION": PRECISION,
+	"FLOAT":     FLOAT,
+	"NUMERIC":   NUMERIC,
+	"DECIMAL":   DECIMAL,
+	"BOOLEAN":   BOOLEAN,
+	"DATE":      DATE,
+	"DATETIME":  DATETIME,
+	"DEFAULT":   DEFAULT,
+	"TRUE":      TRUE,
+	"FALSE":     FALSE,
 }
 
 func Lookup(val string) Type {
