1e63b4bccb05235d35f97e27ec1d527c1483a7e0
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-29 17:31:06 +0330

    get query from input with -q flag

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "flag"
 
+type Query struct {
+	input string
+}
+
+func main() {
+	var query Query
+
+	flag.StringVar(&query.input, "q", "CREATE TABLE test (id INT);", "CREATE TABLE Query that want to validate")
 }
