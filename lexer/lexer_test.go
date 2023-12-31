3e7e1fbc3c640653a144a5d5287e3fb9524b7436
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-31 08:30:49 +0330

    add multiple test for lexer - all tests done

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -90,3 +90,140 @@ func TestFullCreateTableQuery(t *testing.T) {
 		}
 	}
 }
+
+func TestCreatingTableQueries(t *testing.T) {
+	t.Parallel()
+
+	inputs := []string{
+		`CREATE TABLE usernames (
+			username TEXT PRIMARY KEY,
+			password TEXT NOT NULL
+		  );
+		`,
+		`CREATE TABLE products (
+			productID INTEGER PRIMARY KEY,
+			name TEXT NOT NULL UNIQUE,
+			description TEXT,
+			category TEXT
+		  );		  
+		`,
+		`CREATE TABLE orders (
+			orderID INTEGER PRIMARY KEY,
+			customerID INTEGER,
+			productID INTEGER,
+			quantity INTEGER NOT NULL,
+			price REAL NOT NULL
+		  );
+		`,
+	}
+
+	tests := [][]struct {
+		name            string
+		expectedType    token.Type
+		expectedLiteral string
+	}{
+		// CREATE TABLE usernames (
+		// 	username TEXT PRIMARY KEY,
+		// 	password TEXT NOT NULL
+		//   );
+		{
+			{"create keyword", token.CREATE, "CREATE"},
+			{"table keyword", token.TABLE, "TABLE"},
+			{"table name", token.NAME, "usernames"},
+			{"left pranthesis", token.LPARENTHESIS, "("},
+			{"column name", token.NAME, "username"},
+			{"TEXT type", token.TEXT, "TEXT"},
+			{"PRIMARY", token.PRIMARY, "PRIMARY"},
+			{"KEY", token.KEY, "KEY"},
+			{"comma", token.COMMA, ","},
+			{"column name", token.NAME, "password"},
+			{"TEXT type", token.TEXT, "TEXT"},
+			{"NOT", token.NOT, "NOT"},
+			{"NULL", token.NULL, "NULL"},
+			{"right pranthesis", token.RPARENTHESIS, ")"},
+			{"semicolon", token.SEMICOLON, ";"},
+		},
+		// CREATE TABLE products (
+		// 	productID INTEGER PRIMARY KEY,
+		// 	name TEXT NOT NULL UNIQUE,
+		// 	description TEXT,
+		// 	category TEXT
+		//   );
+		{
+			{"CREATE keyword", token.CREATE, "CREATE"},
+			{"TABLE keyword", token.TABLE, "TABLE"},
+			{"table name", token.NAME, "products"},
+			{"left pranthesis", token.LPARENTHESIS, "("},
+			{"column name", token.NAME, "productID"},
+			{"INTEGER", token.INTEGER, "INTEGER"},
+			{"PRIMARY", token.PRIMARY, "PRIMARY"},
+			{"KEY", token.KEY, "KEY"},
+			{"comma", token.COMMA, ","},
+			{"column name", token.NAME, "name"},
+			{"TEXT type", token.TEXT, "TEXT"},
+			{"NOT", token.NOT, "NOT"},
+			{"NULL", token.NULL, "NULL"},
+			{"UNIQUE", token.UNIQUE, "UNIQUE"},
+			{"comma", token.COMMA, ","},
+			{"column name", token.NAME, "description"},
+			{"TEXT type", token.TEXT, "TEXT"},
+			{"comma", token.COMMA, ","},
+			{"column name", token.NAME, "category"},
+			{"TEXT type", token.TEXT, "TEXT"},
+			{"right pranthesis", token.RPARENTHESIS, ")"},
+			{"semicolon", token.SEMICOLON, ";"},
+		},
+		// CREATE TABLE orders (
+		// 	orderID INTEGER PRIMARY KEY,
+		// 	customerID INTEGER,
+		// 	productID INTEGER,
+		// 	quantity INTEGER NOT NULL,
+		// 	price REAL NOT NULL
+		//   );
+		{
+			{"CREATE keyword", token.CREATE, "CREATE"},
+			{"TABLE keyword", token.TABLE, "TABLE"},
+			{"table name", token.NAME, "orders"},
+			{"left pranthesis", token.LPARENTHESIS, "("},
+			{"column name", token.NAME, "orderID"},
+			{"INTEGER", token.INTEGER, "INTEGER"},
+			{"PRIMARY", token.PRIMARY, "PRIMARY"},
+			{"KEY", token.KEY, "KEY"},
+			{"comma", token.COMMA, ","},
+			{"column name", token.NAME, "customerID"},
+			{"INTEGER", token.INTEGER, "INTEGER"},
+			{"comma", token.COMMA, ","},
+			{"column name", token.NAME, "productID"},
+			{"INTEGER", token.INTEGER, "INTEGER"},
+			{"comma", token.COMMA, ","},
+			{"column name", token.NAME, "quantity"},
+			{"INTEGER", token.INTEGER, "INTEGER"},
+			{"NOT", token.NOT, "NOT"},
+			{"NULL", token.NULL, "NULL"},
+			{"comma", token.COMMA, ","},
+			{"column name", token.NAME, "price"},
+			{"REAL", token.REAL, "REAL"},
+			{"NOT", token.NOT, "NOT"},
+			{"NULL", token.NULL, "NULL"},
+			{"right pranthesis", token.RPARENTHESIS, ")"},
+			{"semicolon", token.SEMICOLON, ";"},
+		},
+	}
+
+	for indexInput, input := range inputs {
+		test := tests[indexInput]
+		lex := New(input)
+
+		for j, tt := range test {
+			tk := lex.NextToken()
+
+			if tk.Literal != tt.expectedLiteral {
+				t.Fatalf("tests[%d][%d] incorrect literal. expected=%s, got=%s", indexInput, j, tt.expectedLiteral, tk.Literal)
+			}
+
+			if tk.Type != tt.expectedType {
+				t.Fatalf("tests[%d][%d] incorrect type. expected=%s, got=%s", indexInput, j, tt.expectedType, tk.Type)
+			}
+		}
+	}
+}
