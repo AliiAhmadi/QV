cd1019a90d44fa12aff2cb4af5fcbad3c49e82c8
AliiAhmadi <aliahmadi.github@gmail.com>
2023-12-31 21:13:53 +0330

    abstract syntax tree

--- syntax_tree/ast.go
+++ syntax_tree/ast.go
@@ -3,3 +3,20 @@ package ast
 type Node interface {
 	TokenLiteral() string
 }
+
+type Command interface {
+	Node
+	commandNode()
+}
+
+type Query struct {
+	Command *Command
+}
+
+func (query *Query) TokenLiteral() string {
+	if query.Command != nil {
+		return query.Command.TokenLiteral()
+	} else {
+		return ""
+	}
+}
